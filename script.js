13244a43c04b3ab299ed3ba65d1525261c3112eb
Ayush maurya <Ayushmaurya20004@gmail.com>
2026-06-20 22:33:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const navToggle = document.querySelector(".nav-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+if (navToggle && mainNav) {
+  navToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("open");
+    navToggle.classList.toggle("open");
+  });
+
+  mainNav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mainNav.classList.remove("open");
+      navToggle.classList.remove("open");
+    });
+  });
+}
